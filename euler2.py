8706d6f4d2494083b8487151d9ce25e141ee713e
Maarten Terpstra <maartenlterpstra@gmail.com>
2013-10-24 14:15:20 +0200

    first solutions

---
+++ euler2.py
@@ -0,0 +1,6 @@
+def fibGen(a = -1, b = 1, upto=4000000):
+	while a + b < upto:
+		a, b = b, a + b
+		yield b
+
+print sum(i for i in fibGen() if i%2)
---
+++ euler3.py
@@ -0,0 +1,16 @@
+def isPrime(n):
+	if n < 2: return False
+	if n == 2: return True
+	if n % 2 == 0: return False
+	for i in range(3, n, 2):
+		if n%i == 0: return False
+
+	return True
+
+def listPrimes(upper):
+	return [x for x in range(2, upper + 1) if isPrime(x)]
+
+def getPrimeFactor(n):
+	return max([x for x in range(2, int(n**0.5) + 1) if n % x == 0 and isPrime(x)])
+
+print(getPrimeFactor(600851475143));
