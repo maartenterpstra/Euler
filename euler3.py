35d7a88d04daa0b61b5c9eb108de2397bfc267eb
Maarten Terpstra <maartenlterpstra@gmail.com>
2013-10-24 14:17:38 +0200

    improved prime finding

--- euler3.py
+++ euler3.py
@@ -2,7 +2,7 @@ def isPrime(n):
 	if n < 2: return False
 	if n == 2: return True
 	if n % 2 == 0: return False
-	for i in range(3, n, 2):
+	for i in range(3, int(n**0.5) + 1, 2):
 		if n%i == 0: return False
 
 	return True
