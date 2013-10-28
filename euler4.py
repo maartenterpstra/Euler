d82207f0f72b81ba5089c6e7cc098d8f8993140d
Maarten Terpstra <maartenlterpstra@gmail.com>
2013-10-28 20:16:43 +0100

    Improved exercise 4

--- euler4.py
+++ euler4.py
@@ -1,11 +1,7 @@
+import itertools
+
 def palin(s):
 	return s == s[::-1]
 
-def getList(a, b):
-	l = []
-	for x in range(a, b):
-		for y in range(a, b):
-			l.append((x, y))
-	return l
-
-print max([q * r for (q, r) in [(q, r) for q in range(100, 1000) for r in range(100, 1000)] if palin(str(q*r))])
+r = range(100, 1000)
+print max([q * r for (q, r) in itertools.product(r, r) if palin(str(q*r))])
