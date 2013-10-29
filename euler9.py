51928262a4ce766a727fbc8433970bcb902857fd
Maarten Terpstra <maartenlterpstra@gmail.com>
2013-10-29 17:16:54 +0100

    Found the required triangle

---
+++ euler9.py
@@ -0,0 +1,3 @@
+#through trial-and-error it seems that 426 is the lowest maximum value that will solve this problem
+maxVal = 426
+print next(x*y*z for x in range(maxVal) for y in range(x + 1, maxVal) for z in range(y + 1, maxVal) if x + y + z == 1000 and x*x + y*y == z*z)
