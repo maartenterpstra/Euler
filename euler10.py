dc29322ec23f5abc8e8e9930fe5899a2d036db1c
Maarten Terpstra <maartenlterpstra@gmail.com>
2013-10-29 17:20:40 +0100

    Found the sum of two million primes in less than 5 seconds

---
+++ euler10.py
@@ -0,0 +1,11 @@
+def isPrime(x):
+	if x < 2: return False
+	if x == 2: return True
+	if x % 2 == 0: return False
+	for i in range(3, int(x**0.5) + 1, 2):
+		if x % i == 0:
+			return False
+
+	return True
+
+print sum(x for x in range(2, 2000000) if isPrime(x))
