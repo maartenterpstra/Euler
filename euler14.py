cff392d9124d2d630ad08f32875c09d6ef54e496
Maarten Terpstra <maartenlterpstra@gmail.com>
2013-11-01 13:56:19 +0100

    Speeded it up by a factor ~20 using lookup list

--- euler14.py
+++ euler14.py
@@ -1,9 +1,12 @@
+knownLengths = [-1] * 1000000
 
 def add_item(list, item):
     list.append(item)
     return list
 
 def collatz(n, l):
+	if n < 1000000 and knownLengths[n] != -1:
+		return len(l) + knownLengths[n]
 	l = add_item(l, n)
 	if n == 1:
 		return len(l)
@@ -17,6 +20,7 @@ maxVal = 1
 for x in range(1, 1000000):
 	myList = []
 	lenlist = collatz(x, myList)
+	knownLengths[x] = lenlist 
 	if(lenlist > Max):
 		Max = lenlist
 		maxVal = x
