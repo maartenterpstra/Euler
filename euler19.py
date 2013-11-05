4248ce5ee4b0cff49aefdb0d9c0d38bad327d7c9
Maarten Terpstra <maartenlterpstra@gmail.com>
2013-11-05 15:43:54 +0100

    found alternate solution

--- euler19.py
+++ euler19.py
@@ -1,6 +1,12 @@
 from datetime import date
 
 numMonths = 12
+
+
+#Could also be a oneliner as here, but the other version is more readable
+#print len([d for d in (date(year, month, 1) for year in range(1901, 2001) for month in range(1, numMonths + 1)) if d.weekday() == 6])
+
+
 i = 0
 for year in range(1901, 2001):
 	#plus one for an inclusive range, since the range function is exclusive the upper limit
