643e41baf7fdbad7a30d5d69ef848f4f5f85e9ad
Maarten Terpstra <maartenlterpstra@gmail.com>
2014-05-08 14:54:59 +0200

    Solved the number of sums

---
+++ euler76.py
@@ -0,0 +1,22 @@
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.f(*args)
+        return self.memo[args]
+
+def partition(n, maxVal = None):
+	if n == 0:
+		return 1
+	sum = 0
+	maxVal = n if maxVal == None else maxVal
+	minVal = min(maxVal, n)
+	for i in range(minVal, 0, -1):
+		sum += partition(n - i, i)
+
+	return sum 
+
+partition = Memoize(partition)
+print (partition(100) - 1)
