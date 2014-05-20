d1c58980381ca43bd9ee97a62fc46507846e867b
Maarten Terpstra <maartenlterpstra@gmail.com>
2014-05-20 15:43:08 +0200

    found the limit

---
+++ euler243.py
@@ -0,0 +1,16 @@
+from fractions import Fraction, gcd
+from decimal import Decimal
+
+upperLim = Decimal(Decimal(15499)/Decimal(94744))
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+def findUpper(upperLim):
+	d, s = 1, 1
+	for p in primes:
+		d *= p
+		s *= (p - 1)
+		for i in xrange(2, p):
+			if s * i / (d * i - 1.0) < upperLim:
+				return d * i
+
+print findUpper(upperLim)
