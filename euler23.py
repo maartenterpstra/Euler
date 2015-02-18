37961a6d20d2091733f449c4fb9751542165cbfb
Maarten Terpstra <maarten@localhost.localdomain>
2015-02-18 13:05:19 +0100

    Solved this one

---
+++ euler23.py
@@ -0,0 +1,20 @@
+from sets import Set
+MAX = 20161
+def sumOfDivisors(n):
+	s = 1
+	for i in xrange(2, int(n ** 0.5) + 1):
+		if n % i == 0:
+			s += i
+			if i != n / i:
+				s += n / i
+	return s
+
+abundantNumbers = [x for x in xrange(1, MAX + 1) if sumOfDivisors(x) > x]
+abSums = Set()
+for i in xrange(len(abundantNumbers)):
+	for j in abundantNumbers[i:]:
+		absum = abundantNumbers[i] + j
+		if absum < MAX:
+			abSums.add(absum)
+
+print (MAX*MAX + MAX)/2 - sum(abSums)
